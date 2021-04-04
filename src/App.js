4d47c6aaae54b196632b61b756b04273ade96f46
Beauty Spot <75646530+BeautySpot88@users.noreply.github.com>
2021-04-04 11:51:30 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./book.gif";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="AppLogo" alt="logo" />
+      </header>
+    </div>
+  );
+}
