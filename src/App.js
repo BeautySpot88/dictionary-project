636955db16c7013bb7d75cb379eaf2dc8ef77f81
Beauty Spot <75646530+BeautySpot88@users.noreply.github.com>
2021-04-04 12:50:50 +0100

    Added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./book.gif";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="AppLogo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="AppLogo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Ebele</small>
+        </footer>
+      </div>
     </div>
   );
 }
